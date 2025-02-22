7d6748062bd0f96748657d0e1fca172453f1b6fb
hwanh2 <ymkim2353@gmail.com>
2025-02-22 18:30:50 +0900

    [Gold V] Title: 토마토, Time: 1588 ms, Memory: 346280 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -3,67 +3,48 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt(); // x
-        int n = sc.nextInt(); // y
-        Queue<int[]> queue = new LinkedList<>();
-        int[][] array = new int[n][m];
-        int[][] visited = new int[n][m];
-        int count = 0;
-        int flag = 0;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                array[i][j] = sc.nextInt();
-                if(array[i][j]==1){
-                    queue.offer(new int[]{i, j, 0});
-                    visited[i][j] = 1;
-                }
-                else if(array[i][j]==-1){
-                    visited[i][j] = -1;
-                }
-            }
-        }
+    static Queue<int[]> queue = new LinkedList<>();
+    static int[][] dis;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
+    public static void bfs(int n,int m){
         while(!queue.isEmpty()){
             int[] tmp = queue.poll();
-            if(tmp[0]+1<n && array[tmp[0]+1][tmp[1]]!=-1 && visited[tmp[0]+1][tmp[1]] != 1){
-                queue.offer(new int[]{tmp[0]+1,tmp[1],tmp[2]+1});
-                visited[tmp[0]+1][tmp[1]] = 1;
-                count = tmp[2]+1;
-            }
-            if(tmp[0]-1>=0 && array[tmp[0]-1][tmp[1]]!=-1 && visited[tmp[0]-1][tmp[1]] != 1){
-                queue.offer(new int[]{tmp[0]-1,tmp[1],tmp[2]+1});
-                visited[tmp[0]-1][tmp[1]] = 1;
-                count = tmp[2]+1;
-            }
-            if(tmp[1]+1<m && array[tmp[0]][tmp[1]+1]!=-1 && visited[tmp[0]][tmp[1]+1] != 1){
-                queue.offer(new int[]{tmp[0],tmp[1]+1,tmp[2]+1});
-                visited[tmp[0]][tmp[1]+1] = 1;
-                count = tmp[2]+1;
-            }
-            if(tmp[1]-1>=0 && array[tmp[0]][tmp[1]-1]!=-1 && visited[tmp[0]][tmp[1]-1] != 1){
-                queue.offer(new int[]{tmp[0],tmp[1]-1,tmp[2]+1});
-                visited[tmp[0]][tmp[1]-1] = 1;
-                count = tmp[2]+1;
-            }
-
-        }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(visited[i][j]==0){
-                    flag = 1;
-                    break;
+            int y = tmp[0];
+            int x = tmp[1];
+            for(int i=0; i<4; i++){
+                if(x+dx[i]>=0 && x+dx[i]<m && y+dy[i]>=0 && y+dy[i]<n && dis[y+dy[i]][x+dx[i]]==0){
+                    queue.offer(new int[] {y+dy[i],x+dx[i]});
+                    dis[y+dy[i]][x+dx[i]] = dis[y][x]+1;
                 }
             }
         }
-        if(flag == 0){
-            System.out.println(count);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int result = 0;
+        dis = new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                dis[i][j] = sc.nextInt();
+                if(dis[i][j] == 1){
+                    queue.offer(new int[] {i,j});
+                }
+            }
         }
-        else{
-            System.out.println(-1);
-        }
+        bfs(n,m);
 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(dis[i][j]==0){
+                    System.out.println(-1);
+                    return;
+                }
+                result = Math.max(result,dis[i][j]);
+            }
+        }
+        System.out.println(result-1);
     }
 }
