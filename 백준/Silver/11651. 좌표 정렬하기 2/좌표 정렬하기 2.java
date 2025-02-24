064275b20eb343b81f09f34988cc90626f8865db
hwanh2 <ymkim2353@gmail.com>
2025-02-24 23:36:20 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1876 ms, Memory: 196484 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Point implements Comparable<Point>{
+    public int x,y;
+    public Point(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public int compareTo(Point o) {
+        if(this.y == o.y){
+            return this.x - o.x;
+        }
+        else{
+            return this.y - o.y;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Point> array = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            array.add(new Point(x,y));
+        }
+        Collections.sort(array);
+        for(Point o : array){
+            System.out.println(o.x+" "+o.y);
+        }
+    }
+}
