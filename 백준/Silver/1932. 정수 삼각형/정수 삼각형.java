1440013c5dcd48358f1aa675fd285500f0dace65
hwanh2 <ymkim2353@gmail.com>
2025-02-27 00:03:50 +0900

    [Silver I] Title: 정수 삼각형, Time: 252 ms, Memory: 27632 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[][] array;
+    static int[][] dy;
+    static void solve(int n){
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=i; j++){
+                if(j==0){
+                    dy[i][j] = dy[i-1][j]+array[i][j];
+                }
+                else if(j==i){
+                    dy[i][j] = dy[i-1][j-1]+array[i][j];
+                }
+                else{
+                    if(dy[i-1][j-1]>dy[i-1][j]){
+                        dy[i][j] = dy[i-1][j-1]+array[i][j];
+                    }
+                    else{
+                        dy[i][j] = dy[i-1][j]+array[i][j];
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        array = new int[n][n];
+        dy = new int[n][n];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j=0; j<=i; j++){
+                array[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dy[0][0] = array[0][0];
+        solve(n);
+        int result = 0;
+        for(int i=0; i<n; i++){
+            result = Math.max(dy[n-1][i],result);
+        }
+        System.out.println(result);
+    }
+}
