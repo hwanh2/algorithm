1fc78826e97138c8efa9c5840f7ccee81c32636d
hwanh2 <ymkim2353@gmail.com>
2025-03-03 16:01:16 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1900 ms, Memory: 76436 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] mat;
+    static int[][] distance;
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+    static Queue<int[]> queue = new LinkedList<>();
+    static void bfs(int n,int m){
+        while(!queue.isEmpty()){
+            int[] tmp = queue.poll();
+            int tmpX = tmp[1];
+            int tmpY = tmp[0];
+            for(int i=0; i<4; i++){
+                int x = tmpX+dx[i];
+                int y = tmpY+dy[i];
+                if(x>=0 && x<m && y>=0 && y<n && mat[y][x]==1 && distance[y][x]==-1){
+                    distance[y][x] = distance[tmpY][tmpX]+1;
+                    queue.offer(new int[] {y,x});
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        mat = new int[n][m];
+        distance = new int[n][m];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j=0; j<m; j++){
+                int tmp = Integer.parseInt(st.nextToken());
+                mat[i][j] = tmp;
+                if(tmp==1){
+                    distance[i][j] = -1;
+                }
+                if(tmp==2){
+                    queue.offer(new int[] {i,j});
+                    distance[i][j] = 0;
+                }
+                if(tmp==0){
+                    distance[i][j] = 0;
+                }
+            }
+        }
+        bfs(n,m);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(distance[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
