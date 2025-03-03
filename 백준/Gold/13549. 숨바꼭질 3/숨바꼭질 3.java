3587db20721d97f803191a4d8a339d463463b34e
hwanh2 <ymkim2353@gmail.com>
2025-03-03 12:46:58 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 18424 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] array = new int[100001];
+    static Queue<Integer> queue = new LinkedList<>();
+    static void bfs(int k){
+        while(!queue.isEmpty()){
+            int tmp = queue.poll();
+            if(tmp==k){
+                System.out.println(array[k]-1);
+                return;
+            }
+            if(tmp*2<100001 && array[tmp*2]==0){
+                array[tmp*2] = array[tmp];
+                queue.offer(tmp*2);
+            }
+            if(tmp-1>=0 && array[tmp-1]==0){
+                array[tmp-1] = array[tmp]+1;
+                queue.offer(tmp-1);
+            }
+            if(tmp+1<100001 && array[tmp+1]==0 ){
+                array[tmp+1] = array[tmp]+1;
+                queue.offer(tmp+1);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        if(n==k){
+            System.out.println(0);
+            return;
+        }
+        array[n] = 1;
+        queue.offer(n);
+        bfs(k);
+    }
+}
