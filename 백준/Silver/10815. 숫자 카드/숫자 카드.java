b47734db7e5bcd0c68b100b8a92cadf670f278da
hwanh2 <ymkim2353@gmail.com>
2025-03-06 01:07:35 +0900

    [Silver V] Title: 숫자 카드, Time: 1176 ms, Memory: 105484 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i=0; i<n; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(array);
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=0; i<m; i++){
+            if(Arrays.binarySearch(array,Integer.parseInt(st.nextToken()))>=0){
+                bw.write(1+" ");
+            }
+            else{
+                bw.write(0+" ");
+            }
+        }
+        bw.flush();
+    }
+}
