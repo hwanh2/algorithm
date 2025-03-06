7642fbfe00bff58fd9cfb0c82191068544f68622
hwanh2 <ymkim2353@gmail.com>
2025-03-06 11:07:02 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 116 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,32 +1,27 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static boolean isPrime(int num){
-        if (num < 2) return false;
-        for(int i=2; i<=num/2; i++){
-            if(num%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] visited = new int[n+1];
-        int[] array = new int[n+1];
-        int index = 0;
-        for(int i=2; i<=n; i++){
-            if(isPrime(i) && visited[i]==0){
-                for(int j=1; i*j<=n; j++){
-                    if(visited[i*j]==0){
-                        array[index++] = i*j;
-                        visited[i*j] = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] prime = new int[n+1];
+        int count = 0;
+        for(int i=2; i<=n; i++) {
+            for (int j = i; j <= n; j += i) {
+                if (prime[j] == 0) {
+                    prime[j] = 1;
+                    count++;
+                    if (count == k) {
+                        bw.write(j + "\n");
+                        bw.flush();
+                        return;
                     }
                 }
             }
         }
-        System.out.println(array[k-1]);
     }
 }
