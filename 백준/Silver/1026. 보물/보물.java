9b17c8075a209c279c41b5d42fdecd8d01af44da
hwanh2 <ymkim2353@gmail.com>
2025-03-10 21:51:10 +0900

    [Silver IV] Title: 보물, Time: 120 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -5,24 +5,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        ArrayList<Integer> array1 = new ArrayList<>();
-        ArrayList<Integer> array2 = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int[] array1 = new int[n];
+        Integer[] array2 = new Integer[n];
+        st = new StringTokenizer(br.readLine()," ");
         for(int i=0; i<n; i++){
-            array1.add(Integer.parseInt(st.nextToken()));
+            array1[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine()," ");
         for(int i=0; i<n; i++){
-            array2.add(Integer.parseInt(st.nextToken()));
+            array2[i] = Integer.parseInt(st.nextToken());
         }
-
-        Collections.sort(array1);
-        Collections.sort(array2);
-        Collections.reverse(array2);
+        Arrays.sort(array1);
+        Arrays.sort(array2,Collections.reverseOrder());
         int sum = 0;
         for(int i=0; i<n; i++){
-            sum+=array1.get(i)*array2.get(i);
+            sum+=array1[i]*array2[i];
         }
         bw.write(sum+"\n");
         bw.flush();
