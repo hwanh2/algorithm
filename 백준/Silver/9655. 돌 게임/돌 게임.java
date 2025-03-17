e884be87f2836792ab494e506c101d2fee966cd0
hwanh2 <ymkim2353@gmail.com>
2025-03-17 13:57:12 +0900

    [Silver V] Title: 돌 게임, Time: 112 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -2,17 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static String dp[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        if(n%2==0){
-            bw.write("CY"+"\n");
+        dp = new String[n+1];
+        dp[1] = "SK";
+        for(int i=2; i<=n; i++){
+            if(dp[i-1].equals("SK")){
+                dp[i] = "CY";
+            }
+            else{
+                dp[i] = "SK";
+            }
         }
-        else{
-            bw.write("SK"+"\n");
-        }
+        bw.write(dp[n]+"\n");
         bw.flush();
     }
 }
