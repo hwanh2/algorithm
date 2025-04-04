54150452e04994aabad169931f741d415d6cdf2f
hwanh2 <ymkim2353@gmail.com>
2025-04-04 13:13:32 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,35 +1,37 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dy;
     static int[] array;
-    static void solve(){
-        dy[0] = 1;
-        int result = 0;
-        for(int i=1; i<array.length; i++){
+    static int[] dp;
+    static void solve(int n){
+        dp[0] = 1;
+        for(int i=1; i<n; i++){
             int max = 0;
-            for(int j=i-1; j>=0; j--){
-                if(array[i]>array[j] && dy[j]>max){
-                    max = dy[j];
+            for(int j=0; j<i; j++){
+                if(array[j]<array[i]){
+                    max = Math.max(dp[j],max);
                 }
             }
-            dy[i] = max+1;
-            result = Math.max(dy[i],result);
+            dp[i] = max+1;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        array = new int[n];
+        dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i=0; i<n; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solve(n);
+        int result = dp[0];
+        for(int i=1; i<n; i++){
+            result = Math.max(result,dp[i]);
         }
         System.out.println(result);
     }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        array = new int[n];
-        dy = new int[n];
-        for(int i=0; i<n; i++){
-            array[i] = sc.nextInt();
-        }
-        if(n==1){
-            System.out.println(1);
-            return;
-        }
-        solve();
-    }
 }
+
