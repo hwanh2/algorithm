4a58525d81948dc80185752a6c7cc54e12260742
hwanh2 <ymkim2353@gmail.com>
2025-04-16 09:46:09 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 416 ms, Memory: 38488 KB -BaekjoonHub

---
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> arrayList = new ArrayList<>();
+        Map<String,Integer> map = new LinkedHashMap<>();
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            String str = br.readLine();
+            map.put(str,map.getOrDefault(str,0)+1);
+        }
+        for(int i=0; i<n-1; i++){
+            String str = br.readLine();
+            if(map.get(str)==1){
+                map.remove(str);
+            }
+            else{
+                map.put(str,map.get(str)-1);
+            }
+        }
+        for(String tmp : map.keySet()){
+            sb.append(tmp+"\n");
+        }
+        System.out.println(sb);
+    }
+}
