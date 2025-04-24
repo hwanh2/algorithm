084d953062bddcef36cafdcfc7f0674f74d8256c
hwanh2 <ymkim2353@gmail.com>
2025-04-24 17:23:12 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1052 ms, Memory: 279872 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -3,62 +3,61 @@ import java.util.*;
 
 public class Main {
     static int[] distance;
-    static int[] visited;
     static Queue<Integer> queue;
-    static ArrayList<ArrayList<Integer>> arrayList;
+    static List<List<Integer>> arrayList;
+
     static void shortestPath(int start){
         queue = new LinkedList<>();
         distance[start] = 0;
         queue.offer(start);
+
         while(!queue.isEmpty()){
             int tmp = queue.poll();
-            if(visited[tmp]==0){
-                visited[tmp]=1;
-                for(int edge : arrayList.get(tmp)){
-                    if(distance[tmp]+1<distance[edge]){
-                        distance[edge] = distance[tmp]+1;
-                        queue.offer(edge);
-                    }
+            for(int edge : arrayList.get(tmp)){
+                if(distance[edge] == Integer.MAX_VALUE){
+                    distance[edge] = distance[tmp] + 1;
+                    queue.offer(edge);
                 }
             }
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int start = Integer.parseInt(st.nextToken());
 
-        distance = new int[n+1];
-        visited = new int[n+1];
-        Arrays.fill(distance,Integer.MAX_VALUE);
+        distance = new int[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
 
         arrayList = new ArrayList<>();
-        for(int i=0; i<=n; i++){
+        for(int i = 0; i <= n; i++) {
             arrayList.add(new ArrayList<>());
         }
 
-        for(int i=0; i<m; i++){
-            st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             arrayList.get(a).add(b);
         }
 
         shortestPath(start);
-        int flag = 0;
-        for(int i=1; i<distance.length; i++){
-            if(distance[i]==k){
+
+        for(int i = 1; i <= n; i++) {
+            if(distance[i] == k) {
                 sb.append(i).append("\n");
-                flag = 1;
             }
         }
-        if(flag==0){
+
+        if(sb.length() == 0) {
             System.out.println(-1);
+        } else {
+            System.out.println(sb);
         }
-        System.out.println(sb);
     }
 }
