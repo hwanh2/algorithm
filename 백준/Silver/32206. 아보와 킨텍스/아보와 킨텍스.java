28268f2b0fa4e7188dbfb8ff34d85fc4331f44dc
hwanh2 <ymkim2353@gmail.com>
2025-05-13 12:44:48 +0900

    [Silver I] Title: 아보와 킨텍스, Time: 140 ms, Memory: 17180 KB -BaekjoonHub

---
+++ 백준/Silver/32206. 아보와 킨텍스/아보와 킨텍스.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        System.out.println((str.length()+1)*26-str.length());
+
+
+
+    }
+
+}
