1822ad3d69dadb426c45ed72ed6bb6da54df554c
hwanh2 <ymkim2353@gmail.com>
2025-05-24 18:38:49 +0900

    [Gold IV] Title: 네트워크 연결, Time: 508 ms, Memory: 50284 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+    static List<Edge> arrayList;
+    static int find(int v){
+        if(v==parent[v]){
+            return v;
+        }
+        return parent[v] = find(parent[v]);
+    }
+    static void union(int a,int b){
+        int fa = find(a);
+        int fb = find(b);
+        if(fa!=fb){
+            parent[fa] = fb;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        parent = new int[n+1];
+        for(int i=1; i<=n; i++){
+            parent[i] = i;
+        }
+
+        arrayList = new ArrayList<>();
+        for(int i=0; i<m; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arrayList.add(new Edge(a,b,c));
+        }
+
+        Collections.sort(arrayList);
+
+        int result = 0;
+        int edgeCount = 0;
+        for(Edge edge : arrayList){
+            if(edgeCount==n-1){
+                break;
+            }
+            int fNode1 = find(edge.node1);
+            int fNode2 = find(edge.node2);
+            if(fNode1!=fNode2){
+                union(fNode1,fNode2);
+                result+=edge.weight;
+                edgeCount++;
+            }
+        }
+        System.out.println(result);
+    }
+}
+
+class Edge implements Comparable<Edge>{
+    public int node1;
+    public int node2;
+    public int weight;
+
+    public Edge(int node1, int node2, int weight) {
+        this.node1 = node1;
+        this.node2 = node2;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.weight - o.weight;
+    }
+}
