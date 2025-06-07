276f186b75d49b843069dad7073a2306de3335f2
hwanh2 <ymkim2353@gmail.com>
2025-06-07 19:18:02 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 128 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -7,26 +7,21 @@ class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] array = new int[n+1];
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        for(int i=1; i<=n; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i=1; i<=n; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[][] dp = new int[n+1][n+1];
-        for(int i=1; i<=n; i++){
-            dp[i][0] = 10000;
-        }
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 10000);
+        dp[0]=0;
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(i<j){
-                    dp[i][j] = dp[i][j-1];
-                }
-                else{
-                    dp[i][j] = Math.min(dp[i][j-1],dp[i-j][j]+array[j]);
-                }
+        for (int i=1; i<=n; i++) {
+            for (int j=i; j<=n; j++) { 
+                dp[j] = Math.min(dp[j], dp[j-i] + array[i]);
             }
         }
-        System.out.println(dp[n][n]);
+
+        System.out.println(dp[n]);
     }
 }
