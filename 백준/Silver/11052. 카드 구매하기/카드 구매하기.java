d074c355bbd651e13862354eda64dcba982ee798
hwanh2 <ymkim2353@gmail.com>
2025-06-07 11:45:38 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i=1; i<=n; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n+1];
+        for (int i=1; i<=n; i++) {
+            for (int j=i; j<=n; j++) {
+                dp[j] = Math.max(dp[j], dp[j-i] + array[i]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
