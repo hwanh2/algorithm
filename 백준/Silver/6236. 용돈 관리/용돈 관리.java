4a1563a5310523e1d58b79a8614205afd996dbf7
hwanh2 <ymkim2353@gmail.com>
2025-06-24 17:05:10 +0900

    [Silver I] Title: 용돈 관리, Time: 228 ms, Memory: 22488 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] array = new int[n];
+        int max = 10000*100000, min = 0;
+        for(int i=0; i<n; i++){
+            array[i] = Integer.parseInt(br.readLine());
+            min = Math.max(min,array[i]);
+        }
+        int result = 0;
+        while(min<=max){
+            int count = 1;
+            int mid = (min+max)/2;
+            int nowCost = mid;
+            for(int i=0; i<n; i++){
+                if(nowCost<array[i]){
+                    count++;
+                    nowCost = mid;
+                }
+                nowCost-=array[i];
+
+            }
+            if(m>=count){
+                result = mid;
+                max = mid-1;
+            }
+            else{
+                min = mid+1;
+            }
+        }
+        System.out.println(result);
+    }
+}
