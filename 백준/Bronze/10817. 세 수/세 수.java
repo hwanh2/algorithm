96747b30f50aec6409aa5e99f3e91731d5f30738
Kim Hwanhee <ymkim2353@gmail.com>
2025-07-18 04:49:15 +0900

    [Bronze IV] Title: 세 수, Time: 100 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int[] array = new int[3];
+        array[0] = Integer.parseInt(st.nextToken());
+        array[1] = Integer.parseInt(st.nextToken());
+        array[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(array);
+        System.out.println(array[1]);
+
+    }
+}
