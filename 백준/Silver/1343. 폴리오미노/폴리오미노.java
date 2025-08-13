c10a127391e26786022ac9e37b7bba5e6de79786
Kim Hwanhee <ymkim2353@gmail.com>
2025-08-14 02:02:50 +0900

    [Silver V] Title: 폴리오미노, Time: 140 ms, Memory: 16264 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        while(str.contains("XXXX")){
+            str = str.replace("XXXX","AAAA");
+        }
+
+        while(str.contains("XX")){
+            str = str.replace("XX","BB");
+        }
+
+        if(str.contains("X")){
+            System.out.println(-1);
+            System.exit(0);
+        }
+
+        System.out.println(str);
+
+    }
+}
