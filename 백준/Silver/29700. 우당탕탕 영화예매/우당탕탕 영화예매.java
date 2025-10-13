893674ff83f0b6ec99253f07b112bd8d662ede02
Kim Hwanhee <ymkim2353@gmail.com>
2025-10-14 03:03:25 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 308 ms, Memory: 27676 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
@@ -5,32 +5,25 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        char[][] array = new char[n][m];
-
-        for(int i=0; i<n; i++){
-            String str = br.readLine();
-            for(int j=0; j<m; j++){
-                array[i][j] = str.charAt(j);
-            }
-        }
-
         int result = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=m-k; j++){
-                int flag = 0;
-                for(int l=0; l<k; l++){
-                    if(array[i][j+l]=='1'){
-                        flag = 1;
-                        break;
-                    }
+        for (int i = 0; i < n; i++) {
+            String row = br.readLine();
+            int emptyCount = 0;
+
+            for (int j = 0; j < m; j++) {
+                if (row.charAt(j) == '0') {
+                    emptyCount++;
+                } else {
+                    emptyCount = 0;
                 }
-                if(flag==0){
+
+                if (emptyCount >= k) {
                     result++;
                 }
             }
