c7864d9fd8b42f7395c4d5da13c3cd954d409247
Kim Hwanhee <ymkim2353@gmail.com>
2026-01-22 16:00:09 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 112 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,36 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] array;
-    static int[] dp;
-    static void solve(int n){
-        dp[0] = 1;
-        for(int i=1; i<n; i++){
-            int max = 0;
-            for(int j=0; j<i; j++){
-                if(array[j]<array[i]){
-                    max = Math.max(dp[j],max);
-                }
-            }
-            dp[i] = max+1;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        array = new int[n];
-        dp = new int[n];
+        int[] array = new int[n];
+        int[] lis = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         for(int i=0; i<n; i++){
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        solve(n);
-        int result = dp[0];
+        lis[0] = array[0];
+        int index = 0;
+
         for(int i=1; i<n; i++){
-            result = Math.max(result,dp[i]);
+            if(lis[index]<array[i]){
+                lis[++index] = array[i];
+            }
+            else{
+                int left = 0;
+                int right = index;
+
+                while(left<right){
+                    int mid = (left+right)/2;
+                    if(lis[mid]<array[i]){
+                        left = mid+1;
+                    }
+                    else{
+                        right = mid;
+                    }
+                }
+                lis[left] = array[i];
+            }
         }
-        System.out.println(result);
+        System.out.println(index+1);
     }
 }
-
